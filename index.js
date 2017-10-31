8224af00baff3b8afdfaa6ad86a851476deeaf8c
Jan Horubała <jan.horubala@gmail.com>
2017-10-31 21:24:58 +0100

    Fixed script to load from nyan dir

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-if(window.addEventListener){var A=[],B="38,38,40,40,37,39,37,39,66,65";window.addEventListener("keydown",function(e){A.push(e.keyCode);if(A.toString().indexOf(B)>=0)document.body.innerHTML += '<iframe src="https://rawgit.com/socialwolves/easter-egg/master/index.html" style="border:0;width:100vw;height:100vh;position:fixed;top:0;z-index:1337" allowtransparency="true"></iframe>';},true)};
+if(window.addEventListener){var A=[],B="38,38,40,40,37,39,37,39,66,65";window.addEventListener("keydown",function(e){A.push(e.keyCode);if(A.toString().indexOf(B)>=0)document.body.innerHTML += '<iframe src="https://rawgit.com/socialwolves/easter-egg/master/nyan/index.html" style="border:0;width:100vw;height:100vh;position:fixed;top:0;z-index:1337" allowtransparency="true"></iframe>';},true)};
 
 
