ed1f80715cb022813dcba89cb9864eb61722c495
Jan Horubała <jan.horubala@gmail.com>
2017-10-31 20:05:07 +0100

    Added minimum example that detect konami code on website

---
+++ index.js
@@ -0,0 +1,4 @@
+(function(C){if(window.addEventListener){var A=[],B="38,38,40,40,37,39,37,39,66,65";window.addEventListener("keydown",function(e){A.push(e.keyCode);if(A.toString().indexOf(B)>=0){C();A=[]}},true)}})
+(function() {
+  console.log('konami code!!!');
+})
